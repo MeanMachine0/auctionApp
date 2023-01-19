30780679eafad51e0c848ac514dd40e514c40330
MeanMachineMarc <marcus9981@icloud.com>
2023-01-19 01:57:34 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using MySql.Data.MySqlClient;
+
+
+namespace auctionApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "server=localhost;port=3306;database=auctiondb;uid=root;password=pTHhHFGxB^U5!1UY^22#x0&n;";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            using (MySqlCommand command = new MySqlCommand("SELECT firstName FROM buyers", connection))
+            {
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                            string firstNames = reader.GetString("firstName");
+                            Debug.Print(firstNames);
+                    }
+                }
+            }
+            connection.Close();
+        }
+
+    }
+    }
+}
